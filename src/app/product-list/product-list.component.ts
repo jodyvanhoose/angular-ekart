a7008ba3ea3a930b004024277359dc1bf4d65571
Jody VanHoose <jodyvanhoose80@gmail.com>
2024-02-24 12:46:22 -0500

    ngFor Directive #20

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,38 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  // name = "John Doe"
-  addToCart: number = 0
-  product = {
-    name: 'iPhone X',
-    price: 789,
-    color: 'Black',
-    discount: 8.5,
-    inStock: 10,
-    pImage: '/assets/images/iphone.jpg'
-  }
+  listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah']
   
-  getDiscountedPrice(){
-    return this.product.price - (this.product.price * this.product.discount / 100)
-  }
-
-  onNameChange(event: any){
-    console.log(event)
-    // this.name = event.target.value
-  }
-
-  decrementCartValue(){
-    if(this.addToCart >0){
-      this.addToCart--
-    }
-    
-  }
-
-  incrementCartValue(){
-    if(this.addToCart < this.product.inStock){
-      this.addToCart++
-    }
-    
-  }
 
 }
